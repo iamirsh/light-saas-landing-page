bba4c8c5dab4829b1c6cf627eeed81b9e6585edf
Irshad Ahmad <ahmadirshad96@gmail.com>
2025-05-15 20:24:23 +0530

    chore: add animation to product showcase section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,10 +1,23 @@
+'use client';
+
 import productImage from '@/assets/product-image.png';
 import Image from 'next/image';
 import pyramidImage from '@/assets/pyramid.png';
 import tubeImage from '@/assets/tube.png';
+import {motion, useScroll, useTransform} from 'framer-motion';
+import { useRef } from 'react';
 
-export const ProductShowcase = () => {
-  return <section className='py-24 md:pt-5 md:pb-10 bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] overflow-x-clip'>
+ export const ProductShowcase = () => {
+  const ref = useRef(null);
+
+ const {scrollYProgress} = useScroll({
+  target:ref,
+  offset:['start end', 'end start'],
+ });
+
+ const translateY = useTransform(scrollYProgress,[0,1],[150,-150])
+
+  return <section ref={ref} className='py-24 md:pt-5 md:pb-10 bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] overflow-x-clip'>
     <div className="container">
       <div className='section-heading'>
         <div className='flex justify-center'>
@@ -16,8 +29,12 @@ export const ProductShowcase = () => {
 
       <div className='relative'>
         <Image src={productImage} alt='Product image' className='mt-10 ' />
-        <Image src={pyramidImage} height={262}  width={262} alt='Pyramid image' className='hidden md:block absolute -top-32 -right-36 ' />
-        <Image src={tubeImage} height={248} alt='Tube image' className='hidden md:block absolute bottom-24 -left-36 ' />
+        <motion.img style={{
+          translateY:translateY
+        }} src={pyramidImage.src} height={262}  width={262} alt='Pyramid image' className='hidden md:block absolute -top-32 -right-36 ' />
+        <motion.img style={{
+          translateY:translateY
+        }} src={tubeImage.src} height={248} width={248} alt='Tube image' className='hidden md:block absolute bottom-24 -left-36 ' />
       </div>
     </div>
   </section>;
